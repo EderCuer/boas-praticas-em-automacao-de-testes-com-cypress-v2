e4f27c5889f6fea45b84d6c5bcf43dfb611839f2
EderCuer <ederscuer@hotmail.com>
2023-07-15 16:29:33 -0300

    Aula 3 - Flaky tests

--- cypress/e2e/flaky-test/sample.cy.js
+++ cypress/e2e/flaky-test/sample.cy.js
@@ -1,13 +1,17 @@
 import { faker } from '@faker-js/faker'
 
+
+//Abra o arquivo cypress/e2e/flaky-test/sample.cy.js e em vez de verificar que o componente com o texto Loading ... 
+//é exibido e depois não mais, aguarde por uma requisição do tipo GET para a rota **/search** antes de seguir adiante
+
 describe('Flaky tests bad practice', () => {
   beforeEach(() => {
+    cy.intercept(
+      'GET', 
+      '**/search**',
+      { fixture: 'stories' }
+    ).as('waitSearch')
     cy.visit('https://wlsf82-hacker-stories.web.app')
-
-    cy.contains('p','Loading ...')
-      .should('be.visible')
-    cy.contains('p','Loading ...')
-      .should('not.exist')
   })
 
   Cypress._.times(10, () => {
@@ -15,11 +19,6 @@ describe('Flaky tests bad practice', () => {
       Cypress._.times(6, () => {
         cy.search(faker.random.word())
       })
-
-      cy.contains('p','Loading ...')
-        .should('be.visible')
-      cy.contains('p','Loading ...')
-        .should('not.exist')
 
       cy.get('.last-searches button')
         .should('have.length', 5)
