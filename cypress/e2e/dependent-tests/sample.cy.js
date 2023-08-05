820165d1a2c06f35f9dd5682d6d2fc642df33f55
EderCuer <ederscuer@hotmail.com>
2023-08-05 10:49:58 -0300

    Aula 9

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -1,38 +1,40 @@
 describe('Dependent tests bad practice', () => {
   beforeEach(() => {
-    cy.visit('http://notes-serverless-app.com')
+    cy.visit('http://notes-serverless-app.com/login')
+    cy.intercept('GET', 'https://90xbti2sk5.execute-api.eu-central-1.amazonaws.com/prod/notes').as('getNotes')
+    cy.intercept('POST', 'https://90xbti2sk5.execute-api.eu-central-1.amazonaws.com/prod/notes').as('postNotes')
+    cy.intercept('PUT', 'https://90xbti2sk5.execute-api.eu-central-1.amazonaws.com/prod/notes/**').as('putNotes')
+    cy.intercept('DELETE', 'https://90xbti2sk5.execute-api.eu-central-1.amazonaws.com/prod/notes/**').as('deleteNotes')
 
-    cy.get('.navbar-nav a:contains(Login)').click()
-
-    cy.get('#email').type(Cypress.env('user_email'))
-    cy.get('#password').type(Cypress.env('user_password'), { log: false })
+    cy.get('#email').type(Cypress.env('email'))
+    cy.get('#password').type(Cypress.env('password'), { log: false })
     cy.get('button[type="submit"]').click()
+
+    cy.wait('@getNotes')
     
     cy.contains('h1', 'Your Notes').should('be.visible')
   })
 
-  it('creates a note', () => {
+  it('CRUD note', () => {
+    // Create the note
     cy.contains('Create a new note').click()
-
     cy.get('#content').type('My note')
     cy.contains('Create').click()
-
+    cy.wait('@postNotes')
     cy.get('.list-group').should('contain', 'My note')
-  })
 
-  it('edits a note', () => {
+    // Update the note
     cy.get('.list-group').contains('My note').click()
     cy.get('#content').type(' updated')
     cy.contains('Save').click()
-
+    cy.wait('@putNotes')
     cy.get('.list-group').should('contain', 'My note updated')
     cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
 
-  it('deletes a note', () => {
+    // Delete the note
     cy.get('.list-group').contains('My note updated').click()
     cy.contains('Delete').click()
-
+    cy.wait('@deleteNotes')
     cy.get('.list-group:contains(My note updated)').should('not.exist')
   })
 })
